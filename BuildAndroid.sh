7cec91ceeca4c3c1e61e0f34d155a8ad2f2bea7b
Ken Tsen <tsen1220.us@gmail.com>
2020-02-20 14:25:11 +0800

    BuildTool

---
+++ BuildAndroid.sh
@@ -0,0 +1,10 @@
+UNITY_PATH=/Applications/Unity/Hub/Editor/2019.2.13f1/Unity.app/Contents/MacOS/Unity
+PROJECT_PATH=~/ProjectPath
+BUILD_LOG_PATH=${PROJECT_PATH}/build.log
+DESTINATION_PATH=~/UnityBuildApp
+
+if [ ! -d ${DESTINATION_PATH} ]; then
+  mkdir ${DESTINATION_PATH}
+fi
+
+$UNITY_PATH -quit -batchmode -projectPath ${PROJECT_PATH} -buildTarget Android -executeMethod AndroidShellBuild.Build -logFile ${BUILD_LOG_PATH} -destinationPath ${DESTINATION_PATH}
