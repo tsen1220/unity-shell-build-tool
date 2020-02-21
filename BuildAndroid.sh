0b813d856a85dfe0ece7a5e97148dd9bfcb6b8ad
Ken Tsen <tsen1220.us@gmail.com>
2020-02-21 18:24:27 +0800

    PathSet

--- BuildAndroid.sh
+++ BuildAndroid.sh
@@ -1,7 +1,9 @@
+#!/bin/bash
+
 UNITY_PATH=/Applications/Unity/Hub/Editor/2019.2.13f1/Unity.app/Contents/MacOS/Unity
-PROJECT_PATH=~/ProjectPath
+PROJECT_PATH=/ProjectPath
 BUILD_LOG_PATH=${PROJECT_PATH}/build.log
-DESTINATION_PATH=~/UnityBuildApp
+DESTINATION_PATH=./UnityBuildApp
 
 if [ ! -d ${DESTINATION_PATH} ]; then
   mkdir ${DESTINATION_PATH}
